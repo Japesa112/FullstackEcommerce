1c8b3654737c09b4fe4c62f3e4c2bd12e41eb2bb
Japesa112 <omulodeveloper@gmail.com>
2025-01-25 08:59:58 +0300

    Product screen

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { View} from "react-native";
-import { Box } from "@/components/ui/box";
-import { ButtonText , Button} from "@/components/ui/button";
+import { Pressable, View} from "react-native";
 import { Card, } from "@/components/ui/card";
 import { Image } from "@/components/ui/image";
 import { Text  } from "@/components/ui/text";
 import { Heading } from "@/components/ui/heading";
-import { VStack } from "@/components/ui/vstack";
+import { Link } from "expo-router";
 export default function ProductListItem({ product }: any) {
 
     return (
+      <Link href={`/product/${product.id}`} asChild >
+        <Pressable className="flex-1"> 
         <Card className="p-5 rounded-lg max-w-[360px] flex-1">
+
+          
         <Image
           source={{
             uri: product.image,
@@ -22,27 +24,13 @@ export default function ProductListItem({ product }: any) {
         <Text className="text-sm font-normal mb-2 text-typography-700">
           {product.name}
         </Text>
-        <VStack className="mb-6">
+        
           <Heading size="md" className="mb-4">
             {product.price} 
           </Heading>
-          <Text size="sm">
-            {product.description}   
-          </Text>
-        </VStack>
-        <Box className="flex-col sm:flex-row">
-          <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-            <ButtonText size="sm">Add to cart</ButtonText>
-          </Button>
-          <Button
-            variant="outline"
-            className="px-4 py-2 border-outline-300 sm:flex-1"
-          >
-            <ButtonText size="sm" className="text-typography-600">
-              Wishlist
-            </ButtonText>
-          </Button>
-        </Box>
-      </Card>
+          
+          </Card>
+      </Pressable>
+      </Link>
     );
 }
